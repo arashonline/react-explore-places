ce994c7050c26e755012d1f856092cf9317780f3
rabiei <rabiei@jetsgram.ir>
2020-03-28 06:08:29 +0430

    saving data in db

--- helpers/db.js
+++ helpers/db.js
@@ -18,6 +18,23 @@ export const init = ()=>{
             )
         });
     });
-    return promise; 
-    
+    return promise;
+}
+export const insertPlace = (title,imageUri, address, lat, lng) =>{
+    const promise = new Promise((resolve, reject)=>{
+        db.transaction((tx)=>{
+            tx.executeSql(
+                'INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?,?,?,?,?);',
+            [title,imageUri, address, lat, lng],
+            (_,result)=>{
+                // success function
+                resolve(result);
+            },
+            (_,err)=>{
+                reject(err);
+            }
+            )
+        });
+    });
+    return promise;
 }
--- store/places-actions.js
+++ store/places-actions.js
@@ -1,23 +1,27 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from "expo-file-system";
 
-export const ADD_PLACE = 'ADD_PLACE';
+export const ADD_PLACE = "ADD_PLACE";
+import { insertPlace } from "../helpers/db";
 
 export const addPlace = (title, image) => {
   // using redux thunk
-  return async dispatch =>{
-    const fileName = image.split('/').pop();
+  return async dispatch => {
+    const fileName = image.split("/").pop();
     const newPath = FileSystem.documentDirectory + fileName;
-    
+
     try {
       await FileSystem.moveAsync({
-        from:image,
+        from: image,
         to: newPath
-      })
+      });
+      const dbResult = await insertPlace(title, newPath, "test", 9, 9);
+      console.log(dbResult);
+      dispatch({ type: ADD_PLACE, placeData: {id: dbResult.insertId, title: title, image: newPath } });
     } catch (error) {
       console.log(error);
       throw error;
     }
+
     
-     dispatch({ type: ADD_PLACE, placeData: { title: title, image:newPath} });
-  }
+  };
 };
